dd51ed9349a5c23546e9327d97f0c5153d405b66
Johannes <johannes.mainusch@kommitment.biz>
2018-11-03 11:09:57 +0100

    fixed depßloyment script

--- deploy.sh
+++ deploy.sh
@@ -49,7 +49,7 @@ set +e
 ${SSHSERVER} killall ${TARGETPROGRAM}
 echo killed running ${TARGETPROGRAM}
 set -x
-${SSHSERVER} "./${TARGETPROGRAM} > /tmp/${TARGETPROGRAM}.log 2>&1 &"
+${SSHSERVER} "./${TARGETPROGRAM} -httpPort=20181 -httpsPort=20182 -year=2018> /tmp/${TARGETPROGRAM}.log 2>&1 &"
 ${SSHSERVER} "./${TARGETPROGRAM} -httpPort=20171 -httpsPort=20172 -year=2017 > /tmp/${TARGETPROGRAM}-2017.log 2>&1 &"
 set +x
 set -e
@@ -60,7 +60,7 @@ echo
 echo "filling the crontab @reboot..."
 $SSHSERVER "rm -f crontab.del"
 $SSHSERVER "if crontab -l  | grep -v '{TARGETPROGRAM}' > crontab.del; then echo "crontab exists"; fi"
-$SSHSERVER "echo '@reboot cd /home/$TARGETUSER; ./${TARGETPROGRAM} > /tmp/${TARGETPROGRAM}.log 2>&1 &' >> crontab.del"
+$SSHSERVER "echo '@reboot cd /home/$TARGETUSER; ./${TARGETPROGRAM} -httpPort=20181 -httpsPort=20182 -year=2018> /tmp/${TARGETPROGRAM}.log 2>&1 &' >> crontab.del"
 $SSHSERVER "echo '@reboot cd /home/$TARGETUSER; ./${TARGETPROGRAM} -httpPort=20171 -httpsPort=20172 -year=2017 > /tmp/${TARGETPROGRAM}-2017.log 2>&1 &' >> crontab.del"
 $SSHSERVER "cat crontab.del | crontab -"
 $SSHSERVER "rm -f crontab.del"
