783448f15e562051e0654cbbcae6c4add82d0b11
Johannes <johannes.mainusch@kommitment.biz>
2020-03-24 13:17:35 +0100

    removed log.panic in bookFromTo

--- processing/bookFromTo.go
+++ processing/bookFromTo.go
@@ -3,10 +3,16 @@ package processing
 import (
 	"github.com/ahojsenn/kontrol/account"
 	"github.com/ahojsenn/kontrol/booking"
+	"log"
 )
 
 // book from source to target account
 func bookFromTo (b booking.Booking, source, target *account.Account)  bool {
+
+	if (source == nil || target == nil ) {
+		log.Println("Error: in bookFromTo, got nil account ")
+		return false
+	}
 
 	b.Text += ": "+b.Soll + "-->"+ b.Haben
 	b.Amount *= -1.0 // von soll (negativ)
