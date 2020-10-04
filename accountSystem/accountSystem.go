a9e747d946d824b34bc2d379c08bc402cb62750a
Johannes <johannes.mainusch@kommitment.biz>
2020-10-04 18:25:45 +0200

    added skr03 account für kalkulatorischen Unternehm

--- accountSystem/accountSystem.go
+++ accountSystem/accountSystem.go
@@ -2,13 +2,14 @@ package accountSystem
 
 import (
 	"fmt"
+	"log"
+	"sort"
+	"strconv"
+
 	"github.com/ahojsenn/kontrol/account"
 	"github.com/ahojsenn/kontrol/booking"
 	"github.com/ahojsenn/kontrol/util"
 	"github.com/ahojsenn/kontrol/valueMagnets"
-	"log"
-	"sort"
-	"strconv"
 )
 
 type AccountSystem interface {
@@ -18,11 +19,11 @@ type AccountSystem interface {
 	All() []account.Account
 	Get(id string) (*account.Account, bool)
 	GetSubacc(id string, subacctype string) (*account.Account, bool)
-	CloneAccountsOfType (typ string) []account.Account
+	CloneAccountsOfType(typ string) []account.Account
 	GetSKR03(id string, rownr int) *account.Account
-	GetByType (typ string) map[string]*account.Account
+	GetByType(typ string) map[string]*account.Account
 	ClearBookings()
-	GetAllAccountsOfStakeholder (sh valueMagnets.Stakeholder) []account.Account
+	GetAllAccountsOfStakeholder(sh valueMagnets.Stakeholder) []account.Account
 }
 
 type DefaultAccountSystem struct {
@@ -55,42 +56,41 @@ var SKR03_1900 = account.AccountDescription{Id: "1900", Name: "17_SKR03_1900_Pri
 var SKR03_Umsatzerloese = account.AccountDescription{Id: "SKR03_Umsatzerloese", Name: "1 SKR03_Umsatzerloese 8100-8402", Type: account.KontenartErtrag}
 var SKR03_sonstigeErloese = account.AccountDescription{Id: "SKR03_sonstigeErloese", Name: "2 SKR03_Umsatzerloese 23**-88**", Type: account.KontenartErtrag}
 var SKR03_4100_4199 = account.AccountDescription{Id: "4100_4199", Name: "3 Löhne und Gehälter 4100-4199", Type: account.KontenartAufwand}
+
 //var SKR03_AnlagenabgaengeSachanlagen2310 = account.AccountDescription{Id: "SKR03_AnlagenabgängeSachanlagen-Buchverlust", Name: "4 AnlagenabgängeSachanlagen 2310 (Restb. Buchverlust)", Type: account.KontenartAufwand}
 //var SKR03_AnlagenabgaengeSachanlagen2315 = account.AccountDescription{Id: "SKR03_AnlagenabgängeSachanlagen-Buchgewinn", Name: "4 AnlagenabgängeSachanlagen 2315 (Restb. Buchgewinn)", Type: account.KontenartErtrag}
+var SKR03_kalkKosten = account.AccountDescription{Id: "SKR03_Kostenkalk", Name: "4.1 kalkulatorische Kosten", Type: account.KontenartAufwand}
 var SKR03_Abschreibungen = account.AccountDescription{Id: "SKR03_Abschreibungen", Name: "4 Abschreibungen auf Anlagen 4822-4855", Type: account.KontenartAufwand}
 var SKR03_sonstigeAufwendungen = account.AccountDescription{Id: "SKR03_sonstigeAufwendungen", Name: "5 sonstige Aufwendungen", Type: account.KontenartAufwand}
 var SKR03_Reisekosten = account.AccountDescription{Id: "SKR03_Reisekosten", Name: "5.1 erstattete Reisekosten", Type: account.KontenartAufwand}
 var SKR03_Steuern = account.AccountDescription{Id: "SKR03_Steuern", Name: "6 SKR03_Steuern 4320 Gewerbesteuer", Type: account.KontenartAufwand}
 var ErgebnisNachSteuern = account.AccountDescription{Id: "SKR03_ErgebnisNachSteuern 10000", Name: "7 ErgebnisNachSteuern", Type: account.KontenartVerrechnung}
 
-
 // Verrechnungskonten
 var SKR03_Saldenvortrag = account.AccountDescription{Id: "SKR03_Saldenvortrag", Name: "Saldenvortrag 9000", Type: account.KontenartVerrechnung}
 var SKR03_9790_Restanteil = account.AccountDescription{Id: "SKR03_9790_Restanteil", Name: "SKR03_9790_Restanteil", Type: account.KontenartVerrechnung}
-var SummeAktiva 	= account.AccountDescription{Id: "SummeAktiva", Name: "V: SummeAktiva", Type: account.KontenartVerrechnung}
-var SummePassiva 	= account.AccountDescription{Id: "SummePassiva", Name: "V: SummePassiva", Type: account.KontenartVerrechnung}
+var SummeAktiva = account.AccountDescription{Id: "SummeAktiva", Name: "V: SummeAktiva", Type: account.KontenartVerrechnung}
+var SummePassiva = account.AccountDescription{Id: "SummePassiva", Name: "V: SummePassiva", Type: account.KontenartVerrechnung}
 var AlleKLRBuchungen = account.AccountDescription{Id: "AlleKLRBuchungen", Name: "V: AlleKLRBuchungen", Type: account.KontenartKLR}
 var k_ErloeseVerteilung = account.AccountDescription{Id: "k_ErloeseVerteilung", Name: "V: k_ErloeseVerteilung", Type: account.KontenartVerrechnung}
 
-
 // Unterkonten für kommitmenschen
-var UK_Kosten 				= account.AccountDescription{Id: "_0-Kosten", Name: "", Type: "Aktiv"}
-var UK_AnteileausFairshare 	= account.AccountDescription{Id: "_1-AnteilausFairshare", Name: "", Type: "Aktiv"}
-var UK_Vertriebsprovision 	= account.AccountDescription{Id: "_2-Vertriebsprovision", Name: "", Type: "Aktiv"}
-var UK_AnteilMitmachen 		= account.AccountDescription{Id: "_3-Anteil-Mitmachen", Name: "", Type: "Aktiv"}
-var UK_AnteileAuserloesen 	= account.AccountDescription{Id: "_4-Anteilauserloesen", Name: "", Type: "Aktiv"}
-var UK_Erloese 				= account.AccountDescription{Id: "_5-Erloese", Name: "", Type: "Aktiv"}
+var UK_Kosten = account.AccountDescription{Id: "_0-Kosten", Name: "", Type: "Aktiv"}
+var UK_AnteileausFairshare = account.AccountDescription{Id: "_1-AnteilausFairshare", Name: "", Type: "Aktiv"}
+var UK_Vertriebsprovision = account.AccountDescription{Id: "_2-Vertriebsprovision", Name: "", Type: "Aktiv"}
+var UK_AnteilMitmachen = account.AccountDescription{Id: "_3-Anteil-Mitmachen", Name: "", Type: "Aktiv"}
+var UK_AnteileAuserloesen = account.AccountDescription{Id: "_4-Anteilauserloesen", Name: "", Type: "Aktiv"}
+var UK_Erloese = account.AccountDescription{Id: "_5-Erloese", Name: "", Type: "Aktiv"}
 
-var UK_Entnahmen 			= account.AccountDescription{Id: "_6-Entnahmen", Name: "", Type: "Passiv"}
-var UK_VeraenderungAnlagen 	= account.AccountDescription{Id: "_7-VeränderungAnlagen", Name: "", Type: "Passiv"}
-var UK_AnteilAnAnlagen 		= account.AccountDescription{Id: "_8-AnteilAnAnlagen", Name: "", Type: "Passiv"}
-var UK_Darlehen 			= account.AccountDescription{Id: "_9-Darlehen", Name: "", Type: "Passiv"}
-var UK_LiquidityReserve 	= account.AccountDescription{Id: "_A-Liquiditätsreserve", Name: "", Type: "Passiv"}
-var UK_Verfuegungsrahmen	= account.AccountDescription{Id: "_B-Jahresüberschuss-Bonus", Name: "", Type: "Passiv"}
-var Hauptkonto 				= account.AccountDescription{Id: "Hauptkonto", Name: "", Type: "Hauptkonto"}
+var UK_Entnahmen = account.AccountDescription{Id: "_6-Entnahmen", Name: "", Type: "Passiv"}
+var UK_VeraenderungAnlagen = account.AccountDescription{Id: "_7-VeränderungAnlagen", Name: "", Type: "Passiv"}
+var UK_AnteilAnAnlagen = account.AccountDescription{Id: "_8-AnteilAnAnlagen", Name: "", Type: "Passiv"}
+var UK_Darlehen = account.AccountDescription{Id: "_9-Darlehen", Name: "", Type: "Passiv"}
+var UK_LiquidityReserve = account.AccountDescription{Id: "_A-Liquiditätsreserve", Name: "", Type: "Passiv"}
+var UK_Verfuegungsrahmen = account.AccountDescription{Id: "_B-Jahresüberschuss-Bonus", Name: "", Type: "Passiv"}
+var Hauptkonto = account.AccountDescription{Id: "Hauptkonto", Name: "", Type: "Hauptkonto"}
 
-
-var UK  = [...]account.AccountDescription {
+var UK = [...]account.AccountDescription{
 	UK_Kosten,
 	UK_AnteileausFairshare,
 	UK_AnteilMitmachen,
@@ -104,7 +104,6 @@ var UK  = [...]account.AccountDescription {
 	UK_LiquidityReserve,
 	UK_Verfuegungsrahmen,
 }
-
 
 type Accountlist struct {
 }
@@ -121,8 +120,8 @@ func (this Accountlist) All() []account.AccountDescription {
 		SKR03_1600,
 		SKR03_1900,
 		SKR03_4100_4199,
-//		SKR03_AnlagenabgaengeSachanlagen2310,
-//		SKR03_AnlagenabgaengeSachanlagen2315,
+		//		SKR03_AnlagenabgaengeSachanlagen2310,
+		//		SKR03_AnlagenabgaengeSachanlagen2315,
 		SKR03_sonstigeAufwendungen,
 		SKR03_Reisekosten,
 		SKR03_Anlagen,
@@ -144,8 +143,10 @@ func (this Accountlist) All() []account.AccountDescription {
 		SKR03_9790_Restanteil,
 		AlleKLRBuchungen,
 		k_ErloeseVerteilung,
+		SKR03_kalkKosten,
 	}
 }
+
 /*
 func EmptyDefaultAccountSystem() AccountSystem {
 	o := account.AccountDescription{Id: "all", Name: "Hauptbuch", Type: account.KontenartVerrechnung}
@@ -155,12 +156,12 @@ func EmptyDefaultAccountSystem() AccountSystem {
 func NewDefaultAccountSystem() AccountSystem {
 
 	ad := account.AccountDescription{Id: "all", Name: "Hauptbuch_thisYear", Type: account.KontenartVerrechnung}
-	id_thisYear := fmt.Sprintf("all_%d",util.Global.FinancialYear)
-	ad_thisYear := account.AccountDescription{Id: id_thisYear , Name: "Hauptbuch", Type: account.KontenartVerrechnung}
+	id_thisYear := fmt.Sprintf("all_%d", util.Global.FinancialYear)
+	ad_thisYear := account.AccountDescription{Id: id_thisYear, Name: "Hauptbuch", Type: account.KontenartVerrechnung}
 	accountSystem := DefaultAccountSystem{
 		collectiveAccount_thisYear: &account.Account{Description: ad_thisYear},
 		collectiveAccount_allYears: &account.Account{Description: ad},
-		accounts: make(map[string]*account.Account),
+		accounts:                   make(map[string]*account.Account),
 	}
 
 	// generate accounts according to the AccountList
@@ -179,7 +180,7 @@ func NewDefaultAccountSystem() AccountSystem {
 		// create subaccounts
 		for _, uk := range UK {
 			//			sa := account.AccountDescription{Id: sh.Id+ukname, Name: sh.Name+ukname, Type: valueMagnets.StakeholderTypeKUA}
-			sa := account.AccountDescription{Id: sh.Id+uk.Id, Name: sh.Name+uk.Id, Type: uk.Type}
+			sa := account.AccountDescription{Id: sh.Id + uk.Id, Name: sh.Name + uk.Id, Type: uk.Type}
 			sa.Superaccount = ad.Id
 			accountSystem.Add(account.NewAccount(sa))
 		}
@@ -209,10 +210,6 @@ func (r *DefaultAccountSystem) All() []account.Account {
 	return result
 }
 
-
-
-
-
 func (r *DefaultAccountSystem) Get(id string) (*account.Account, bool) {
 	if a, ok := r.accounts[id]; ok {
 		return a, true
@@ -220,23 +217,21 @@ func (r *DefaultAccountSystem) Get(id string) (*account.Account, bool) {
 	return nil, false
 }
 
-
 func (r *DefaultAccountSystem) CloneAccountsOfStakeholder(sh valueMagnets.Stakeholder) []account.Account {
 	var accounts []account.Account
 
-	acc,_ := r.Get(sh.Id)
+	acc, _ := r.Get(sh.Id)
 	log.Println("in CloneAccountsOfStakeholder", acc)
 	accounts = append(accounts, *acc)
 
 	// find subaccounts
 	for _, uk := range UK {
-		sa := account.AccountDescription{Id: sh.Id+uk.Id, Name: sh.Name+uk.Id, Type: uk.Type}
+		sa := account.AccountDescription{Id: sh.Id + uk.Id, Name: sh.Name + uk.Id, Type: uk.Type}
 		log.Println("in CloneAccountsOfStakeholder", sa)
 	}
 
 	return accounts
 }
-
 
 func (r *DefaultAccountSystem) GetSubacc(id string, subacctype string) (*account.Account, bool) {
 	if a, ok := r.accounts[id+subacctype]; ok {
@@ -246,9 +241,8 @@ func (r *DefaultAccountSystem) GetSubacc(id string, subacctype string) (*account
 	return nil, false
 }
 
-
 func (as *DefaultAccountSystem) GetByType(typ string) map[string]*account.Account {
-	filtered  := make (map[string]*account.Account)
+	filtered := make(map[string]*account.Account)
 
 	for _, account := range as.accounts {
 		if account.Description.Type == typ {
@@ -259,9 +253,8 @@ func (as *DefaultAccountSystem) GetByType(typ string) map[string]*account.Accoun
 	return filtered
 }
 
-
-func (as *DefaultAccountSystem) CloneAccountsOfType (typ string) []account.Account {
-	var filtered  []account.Account
+func (as *DefaultAccountSystem) CloneAccountsOfType(typ string) []account.Account {
+	var filtered []account.Account
 	for _, account := range as.accounts {
 		if account.Description.Type == typ {
 			clone := *account
@@ -271,7 +264,6 @@ func (as *DefaultAccountSystem) CloneAccountsOfType (typ string) []account.Accou
 	}
 	return filtered
 }
-
 
 func (r *DefaultAccountSystem) ClearBookings() {
 	r.collectiveAccount_thisYear.Bookings = []booking.Booking{}
@@ -285,14 +277,13 @@ func (r *DefaultAccountSystem) ClearBookings() {
 	}
 }
 
-
 // get all accounts and subaccounts of a given Stakeholder or empty
-func  (as *DefaultAccountSystem) GetAllAccountsOfStakeholder (sh valueMagnets.Stakeholder) []account.Account {
+func (as *DefaultAccountSystem) GetAllAccountsOfStakeholder(sh valueMagnets.Stakeholder) []account.Account {
 	var accountlist []account.Account
 	var stakeholder valueMagnets.Stakeholder
 
 	// check if stakeholder is in sztakeholder list
-	for _,s := range stakeholder.All(util.Global.FinancialYear){
+	for _, s := range stakeholder.All(util.Global.FinancialYear) {
 		if s == sh {
 			// now add all the beloging accounts...
 			for _, account := range as.accounts {
@@ -310,13 +301,10 @@ func  (as *DefaultAccountSystem) GetAllAccountsOfStakeholder (sh valueMagnets.St
 	return accountlist
 }
 
-
-
-
 // find the right account for the SKR03konto string
 func (r *DefaultAccountSystem) GetSKR03(SKR03konto string, rownr int) *account.Account {
 	var account *account.Account
-	switch  {
+	switch {
 	case IsInRange(SKR03konto, 25, 35): // Anlage buchen
 		account = r.accounts[SKR03_Anlagen25_35.Id]
 	case IsInRange(SKR03konto, 300, 490): // Anlage buchen
@@ -345,36 +333,38 @@ func (r *DefaultAccountSystem) GetSKR03(SKR03konto string, rownr int) *account.A
 		account = r.accounts[SKR03_1600.Id]
 	case IsInRange(SKR03konto, 1700, 1754),
 		IsInRange(SKR03konto, 1758, 1799):
-		account =  r.accounts[SKR03_sonstVerb.Id]  // bspw. 1755
+		account = r.accounts[SKR03_sonstVerb.Id] // bspw. 1755
 	case IsInRange(SKR03konto, 1769, 1791):
 		account = r.accounts[SKR03_Umsatzsteuer.Id]
 	case SKR03konto == "1900":
 		account = r.accounts[SKR03_1900.Id]
-//	case IsInRange(SKR03konto, 2310, 2313):  --> ist nun in sonstigen Aufwendungen
-//		account = r.accounts[SKR03_AnlagenabgaengeSachanlagen2310.Id]
+		//	case IsInRange(SKR03konto, 2310, 2313):  --> ist nun in sonstigen Aufwendungen
+		//		account = r.accounts[SKR03_AnlagenabgaengeSachanlagen2310.Id]
 	case SKR03konto == "4120":
 		account = r.accounts[SKR03_4100_4199.Id]
 	case IsInRange(SKR03konto, 1755, 1756),
-		IsInRange(SKR03konto, 4130, 4140):
-		account = r.accounts[SKR03_4100_4199.Id]  // Löhne & Gehälter
+		IsInRange(SKR03konto, 4100, 4140):
+		account = r.accounts[SKR03_4100_4199.Id] // Löhne & Gehälter
 	case SKR03konto == "4320",
 		IsInRange(SKR03konto, 2200, 2289):
 		account = r.accounts[SKR03_Steuern.Id]
 	case IsInRange(SKR03konto, 4822, 4855):
 		account = r.accounts[SKR03_Abschreibungen.Id]
+	case SKR03konto == "2890":
+		account = r.accounts[SKR03_kalkKosten.Id]
 	case IsInRange(SKR03konto, 2000, 2199),
-		 IsInRange(SKR03konto, 2300, 2309),
-		 IsInRange(SKR03konto, 2310, 2313),
-		 IsInRange(SKR03konto, 2320, 2350),
-		 IsInRange(SKR03konto, 2380, 2409),
-		 IsInRange(SKR03konto, 3300, 3735),
+		IsInRange(SKR03konto, 2300, 2309),
+		IsInRange(SKR03konto, 2310, 2313),
+		IsInRange(SKR03konto, 2320, 2350),
+		IsInRange(SKR03konto, 2380, 2409),
+		IsInRange(SKR03konto, 3300, 3735),
 		IsInRange(SKR03konto, 3737, 3999),
-		 IsInRange(SKR03konto, 4200, 4306),
-		 IsInRange(SKR03konto, 4360, 4500),
-		 IsInRange(SKR03konto, 4520, 4810),
- 		 IsInRange(SKR03konto, 4886, 4887),
-		 IsInRange(SKR03konto, 4900, 4980),
-		 IsInRange(SKR03konto, 8800, 8819):
+		IsInRange(SKR03konto, 4200, 4306),
+		IsInRange(SKR03konto, 4360, 4500),
+		IsInRange(SKR03konto, 4520, 4810),
+		IsInRange(SKR03konto, 4886, 4887),
+		IsInRange(SKR03konto, 4900, 4999),
+		IsInRange(SKR03konto, 8800, 8819):
 		account = r.accounts[SKR03_sonstigeAufwendungen.Id]
 	case IsInRange(SKR03konto, 4660, 4681):
 		account = r.accounts[SKR03_Reisekosten.Id]
@@ -399,14 +389,13 @@ func (r *DefaultAccountSystem) GetSKR03(SKR03konto string, rownr int) *account.A
 	case SKR03konto == "10000":
 		account = r.accounts[ErgebnisNachSteuern.Id]
 	default:
-		err := fmt.Sprintf( "GetSKR03: could not process booking type '%s' in row %d", SKR03konto, rownr)
-		log.Printf( err)
+		err := fmt.Sprintf("GetSKR03: could not process booking type '%s' in row %d", SKR03konto, rownr)
+		log.Printf(err)
 		util.Global.Errors = append(util.Global.Errors, err)
 		// panic(fmt.Sprintf("GetSKR03: SKR03Bucket/Stakeholder/Konto '%s' not found", account.Description))
 	}
 	return account
 }
-
 
 // check if an SKR03 account number is in range
 func IsInRange(num string, low, high int) bool {
